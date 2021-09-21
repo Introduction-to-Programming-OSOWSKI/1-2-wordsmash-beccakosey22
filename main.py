0050747b919a06c7f2ca433ca161eb1418fc3468
beccakosey22 <820512@mystma.org>
2021-09-21 09:18:39 -0500

    3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #create a function named wordSmash() that combines two variables into one word
 def wordSmash(a, b):
-    print(a + b)
+    return a + b
 
-wordSmash("Cat", "Dog")
+print(wordSmash("Cat", "Dog"))
